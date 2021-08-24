cda77dcd830a105cf9d34a7c53d0e4c4b54f5f29
Gouldsc <gouldsc@mac.com>
2021-08-24 12:03:48 -0400

    Initial Commit

---
+++ Weekly Schedule Selector/Weekly_Schedule_SelectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weekly_Schedule_SelectorApp.swift
+//  Weekly Schedule Selector
+//
+//  Created by Scott Gould on 8/24/21.
+//
+
+import SwiftUI
+
+@main
+struct Weekly_Schedule_SelectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
